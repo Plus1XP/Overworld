e570704fb3e09a5e07d216e96a0bc492163ebe5a
Plus1XP <evldev@protonmail.ch>
2019-02-05 17:03:51 +0000

    Update scene

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -7,6 +7,9 @@ public class CameraMovement : MonoBehaviour
     public Transform Target;
     public float Smoothing = 0.6f;
 
+    public Vector2 MaxPosition;
+    public Vector2 MinPosition;
+
 	// Use this for initialization
 	void Start ()
     {
@@ -19,6 +22,8 @@ public class CameraMovement : MonoBehaviour
         if (transform.position != Target.position)
         {
             Vector3 targetPosition = new Vector3(Target.position.x, Target.position.y, transform.position.z);
+            targetPosition.x = Mathf.Clamp(targetPosition.x, MinPosition.x, MaxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, MinPosition.y, MaxPosition.y);
             transform.position = Vector3.Lerp(transform.position, targetPosition, Smoothing);
         }
 	}
