37070b7526d6762948c9fbe78481e9c164acd499
Plus1XP <evldev@protonmail.ch>
2019-02-09 15:18:13 +0000

    Add enemy AI
    - Add enemy wake
    - Add enemy follow
    - Add enemy knockback

---
+++ Assets/Scripts/EnemyKnockback.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyKnockback : MonoBehaviour
+{
+    public float KnockbackForce;
+    public float KnockbackTime;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Checks if object tagged with "Enemy" is in trigger zone
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody2D Enemy = collision.GetComponent<Rigidbody2D>();
+
+            // Checks if enemy has a rigidbody
+            if (Enemy != null)
+            {
+                // Disable kinematic so it will take the values from this script
+                Enemy.isKinematic = false;
+                Vector2 difference = Enemy.transform.position - transform.position;
+                
+                // Normailizing the vector so it has a length of 1 so it doesnt move to fast
+                difference = difference.normalized * KnockbackForce;
+                Enemy.AddForce(difference, ForceMode2D.Impulse);
+                StartCoroutine(KnockCo(Enemy));
+            }
+        }
+    }
+
+    // Coroutine checks the enemy isnt dead then stops the enemy from moving after a set amount of time
+    private IEnumerator KnockCo(Rigidbody2D enemy)
+    {
+        // Checks the enemy isnt dead
+        if (enemy != null)
+        {
+            yield return new WaitForSeconds(KnockbackTime);
+
+            // Stops enemy from moving by resetting velocity
+            enemy.velocity = Vector2.zero;
+            enemy.isKinematic = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Log.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : Enemy
+{
+    public Transform target;
+    public float ChaseRadius;
+    public float AttackRadius;
+    public Transform HomePosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Find the location, scale, rotation & position of the object tagged with "Player"
+        target = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckDistance();
+	}
+
+    void CheckDistance()
+    {
+        // Finds the distance from the player to the log
+        if (Vector3.Distance(target.position, transform.position) <= ChaseRadius && Vector2.Distance(target.position, transform.position) > AttackRadius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, MoveSpeed * Time.deltaTime);
+        }
+    }
+}
