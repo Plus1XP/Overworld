e95e8556d2eb8d24eb2d641e14c0d9a0ea901180
Plus1XP <evldev@protonmail.ch>
2019-02-19 19:34:55 +0000

    Add signal system for scriptable objects

---
+++ Assets/Scripts/ScriptableObjects/Signal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+// Sits in memory to tell the signallistner what to do
+public class Signal : ScriptableObject
+{
+    // List of what is listening to this class
+    public List<SignalListener> Listeners = new List<SignalListener>();
+
+    public void Raise()
+    {
+        // Loops through listners and raises a method
+        // Loops backwards incase something is removed, it will not cause an error
+        for (int i = Listeners.Count -1; i >= 0; i--)
+        {
+            Listeners[i].OnSignalRaised();
+        }
+    }
+
+    public void RegisterListener(SignalListener listner)
+    {
+        // Registers what is listening to the list
+        Listeners.Add(listner);
+    }
+
+    public void DeRegisterListener(SignalListener listener)
+    {
+        // Deregisters listeners from the list
+        Listeners.Remove(listener);
+    }
+}
---
+++ Assets/Scripts/SignalListener.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+// Attaches to game objects waiting for signal to send commands
+public class SignalListener : MonoBehaviour
+{
+    public Signal Signal;
+    public UnityEvent SignalEvent;
+
+    public void OnSignalRaised()
+    {
+        // Calls wahat ever the event is
+        SignalEvent.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        // registers method on signal class
+        Signal.RegisterListener(this);        
+    }
+
+    private void OnDisable()
+    {
+        // deregisters method on signal class
+        Signal.DeRegisterListener(this);
+    }
+}
